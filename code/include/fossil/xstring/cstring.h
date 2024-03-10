7b4fe5273c254fd61153451fe04f52222c102c72
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-10 18:23:38 -0500

    Update cstring.h

--- code/include/fossil/xstring/cstring.h
+++ code/include/fossil/xstring/cstring.h
@@ -24,15 +24,69 @@ typedef const char **const_cstrings;
 typedef const char *const_cstring;
 
 // =================================================================
-// Available functions
+// String and Strings Management Functions
 // =================================================================
+
+/**
+ * Create a mutable string with the specified initial value.
+ *
+ * @param initial_value The initial value for the string.
+ * @return The created mutable string.
+ */
 cstring fscl_string_create(const char* initial_value);
+
+/**
+ * Create an array of mutable strings with the specified size.
+ *
+ * @param size The size of the array.
+ * @return The created array of mutable strings.
+ */
 cstrings fscl_strings_create(int size);
+
+/**
+ * Create an immutable string with the specified initial value.
+ *
+ * @param initial_value The initial value for the string.
+ * @return The created immutable string.
+ */
 const_cstring fscl_const_string_create(const char* initial_value);
+
+/**
+ * Create an array of immutable strings with the specified size.
+ *
+ * @param size The size of the array.
+ * @return The created array of immutable strings.
+ */
 const_cstrings fscl_const_strings_create(int size);
+
+/**
+ * Erase a mutable string and free its memory.
+ *
+ * @param str The mutable string to be erased.
+ */
 void fscl_string_erase(cstring str);
+
+/**
+ * Erase an array of mutable strings and free their memory.
+ *
+ * @param strings The array of mutable strings to be erased.
+ * @param size    The size of the array.
+ */
 void fscl_strings_erase(cstrings strings, int size);
+
+/**
+ * Erase an immutable string (no memory deallocation as it's assumed to be a constant).
+ *
+ * @param str The immutable string to be erased.
+ */
 void fscl_const_string_erase(const_cstring str);
+
+/**
+ * Erase an array of immutable strings (no memory deallocation as they're assumed to be constants).
+ *
+ * @param strings The array of immutable strings to be erased.
+ * @param size    The size of the array.
+ */
 void fscl_const_strings_erase(const_cstrings strings, int size);
 
 #ifdef __cplusplus
