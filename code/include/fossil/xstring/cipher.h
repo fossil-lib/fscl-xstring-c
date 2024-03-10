8a91b59c9249d2c4bfe94a3d96fabcec087d005b
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-03-10 18:17:30 -0500

    Update cipher.h

--- code/include/fossil/xstring/cipher.h
+++ code/include/fossil/xstring/cipher.h
@@ -23,25 +23,145 @@ typedef char *cipher;
 typedef const char *const_cipher;
 
 // =================================================================
-// Available functions
+// Available Functions
 // =================================================================
+
+/**
+ * Encrypt a message using the Caesar cipher.
+ *
+ * @param message The message to encrypt.
+ * @param shift   The shift value for encryption.
+ */
 void fscl_cipher_caesar_encrypt(cipher message, int shift);
+
+/**
+ * Decrypt a message encrypted with the Caesar cipher.
+ *
+ * @param message The message to decrypt.
+ * @param shift   The shift value for decryption.
+ */
 void fscl_cipher_caesar_decrypt(cipher message, int shift);
+
+/**
+ * Encrypt a message using the Atbash cipher.
+ *
+ * @param message The message to encrypt.
+ */
 void fscl_cipher_atbash_encrypt(cipher message);
+
+/**
+ * Decrypt a message encrypted with the Atbash cipher.
+ *
+ * @param message The message to decrypt.
+ */
 void fscl_cipher_atbash_decrypt(cipher message);
+
+/**
+ * Encrypt a message using the Substitution cipher with a given key.
+ *
+ * @param message The message to encrypt.
+ * @param key     The substitution key.
+ */
 void fscl_cipher_substitution_encrypt(cipher message, const_cipher key);
+
+/**
+ * Decrypt a message encrypted with the Substitution cipher with a given key.
+ *
+ * @param message The message to decrypt.
+ * @param key     The substitution key.
+ */
 void fscl_cipher_substitution_decrypt(cipher message, const_cipher key);
+
+/**
+ * Encrypt a message using the Haxor cipher.
+ *
+ * @param message The message to encrypt.
+ */
 void fscl_cipher_haxor_encrypt(cipher message);
+
+/**
+ * Decrypt a message encrypted with the Haxor cipher.
+ *
+ * @param message The message to decrypt.
+ */
 void fscl_cipher_haxor_decrypt(cipher message);
+
+/**
+ * Encrypt a message using the Morse code.
+ *
+ * @param message The message to encrypt.
+ */
 void fscl_cipher_morse_encrypt(cipher message);
+
+/**
+ * Decrypt a message encrypted with the Morse code.
+ *
+ * @param message The message to decrypt.
+ */
 void fscl_cipher_morse_decrypt(cipher message);
+
+/**
+ * Encrypt a message using the ROT13 cipher.
+ *
+ * @param message The message to encrypt.
+ */
 void fscl_cipher_rot13_encrypt(cipher message);
+
+/**
+ * Decrypt a message encrypted with the ROT13 cipher.
+ *
+ * @param message The message to decrypt.
+ */
 void fscl_cipher_rot13_decrypt(cipher message);
+
+/**
+ * Encrypt a message using the Playfair cipher with a given key.
+ *
+ * @param message    The message to encrypt.
+ * @param key        The Playfair key.
+ * @param encrypted  The encrypted message.
+ */
 void fscl_cipher_playfair_encrypt(cipher message, cipher key, cipher encrypted);
+
+/**
+ * Decrypt a message encrypted with the Playfair cipher with a given key.
+ *
+ * @param message    The message to decrypt.
+ * @param key        The Playfair key.
+ * @param decrypted  The decrypted message.
+ */
 void fscl_cipher_playfair_decrypt(cipher message, cipher key, cipher decrypted);
+
+/**
+ * Encrypt a message using the Rail Fence cipher with a given number of rails.
+ *
+ * @param message The message to encrypt.
+ * @param rails   The number of rails in the Rail Fence.
+ */
 void fscl_cipher_rail_fence_encrypt(cipher message, int rails);
+
+/**
+ * Decrypt a message encrypted with the Rail Fence cipher with a given number of rails.
+ *
+ * @param message The message to decrypt.
+ * @param rails   The number of rails in the Rail Fence.
+ */
 void fscl_cipher_rail_fence_decrypt(cipher message, int rails);
+
+/**
+ * Encrypt a message using the Vigenere cipher with a given key.
+ *
+ * @param message The message to encrypt.
+ * @param key     The Vigenere key.
+ */
 void fscl_cipher_vigenere_encrypt(cipher message, const_cipher key);
+
+/**
+ * Decrypt a message encrypted with the Vigenere cipher with a given key.
+ *
+ * @param message The message to decrypt.
+ * @param key     The Vigenere key.
+ */
 void fscl_cipher_vigenere_decrypt(cipher message, const_cipher key);
 
 #ifdef __cplusplus
